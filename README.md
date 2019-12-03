0d4df2cac1ad90a0ed0b8ac32f411c9b640b79f3
kgc0019 <kgc0019@tigermail.auburn.edu>
2019-12-03 10:32:40 -0600

    Can now chose to build C or Java Server

--- README.md
+++ README.md
@@ -3,9 +3,12 @@
 To compile and run the TCP calculation client and server, follow these instructions.
 
 1. Unzip the folder and cd into it
-2. Run the *setup.sh* script (`./setup.sh`)
+2. Run the *setup.sh* script (`./setup.sh`) this will build the java server by
+default
+	- If you want to build with the C server instead, use `./setup.sh --cserv`
 3. cd into the newly created build directory (`cd build`)
 3. Run the Java server by typing `Java ServerTCP [port]`
+	- If you built with --cserv, the syntax is `./ClientTCP [port]`
 4. Run the client by typing `./ClientTCP [server address] [port]`
 
 And then just use the calculation as outlined in the project description.
--- setup.sh
+++ setup.sh
@@ -7,6 +7,7 @@ if [ -d "$BUILDDIR" ]; then
 fi
 mkdir ${DIR}/build
 cd $DIR/build
+if [[  ( "$#" -ge 1  &&  "$1" == "--java" )  || "$#" < 1 ]]; then
 printf 'Building Java TCP server: '
 (while :; do for c in / - \\ \|; do printf '\b%s' "$c"; sleep 1; done; done) &
 cp ${DIR}/ServerFiles/* ${BUILDDIR}
@@ -14,10 +15,20 @@ sleep 3
 javac ServerTCP.java > /dev/null
 { printf '\n'; kill $! && wait $!; } 2>/dev/null
 printf 'Java Server Built.\n\n'
+fi
+if [ $# -ge 1 ] && [ "$1" == "--cserv" ] ; then
+printf 'Configuring and building C TCP client and Server: '
+(while :; do for c in / - \\ \|; do printf '\b%s' "$c"; sleep 1; done; done) &
+sleep 3
+cmake -DCSERVERBUILD=ON .. > /dev/null
+make >/dev/null
+{ printf '\n'; kill $! && wait $!; } 2>/dev/null
+else 
 printf 'Configuring and building C TCP client: '
 (while :; do for c in / - \\ \|; do printf '\b%s' "$c"; sleep 1; done; done) &
 sleep 3
 cmake .. > /dev/null
 make >/dev/null
 { printf '\n'; kill $! && wait $!; } 2>/dev/null
+fi
 printf 'C Client Built.\n\n'
